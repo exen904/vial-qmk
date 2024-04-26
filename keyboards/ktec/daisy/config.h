26e9d3284e310538e9cc4317bc6b55befad9203a
exen904 <diesdas@kurantrubys.de>
2024-04-26 16:23:48 +0200

    daisy_vial_port

--- keyboards/ktec/daisy/config.h
+++ keyboards/ktec/daisy/config.h
@@ -17,21 +17,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+/*
 #    define RGBLED_NUM 8
 #    define RGBLIGHT_HUE_STEP 12
 #    define RGBLIGHT_SAT_STEP 25
-#    define RGBLIGHT_VAL_STEP 12
-//   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-#    define RGBLIGHT_EFFECT_BREATHING
-#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#    define RGBLIGHT_EFFECT_SNAKE
-#    define RGBLIGHT_EFFECT_KNIGHT
-#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#    define RGBLIGHT_EFFECT_RGB_TEST
-#    define RGBLIGHT_EFFECT_ALTERNATING
-#    define RGBLIGHT_EFFECT_TWINKLE
+#    define RGBLIGHT_VAL_STEP 12 
+#    define RGBLIGHT_SLEEP   If defined, the RGB lighting will be switched off when the host goes to sleep 
+*/
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
@@ -52,4 +44,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* disable action features */
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+#define NO_ACTION_ONESHOT
