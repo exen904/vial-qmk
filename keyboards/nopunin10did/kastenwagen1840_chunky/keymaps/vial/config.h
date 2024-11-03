fa62f5b89194dc0a65e68b196b2d1ff3dcfa4e8c
exen904 <diesdas@kurantrubys.de>
2024-11-03 18:10:52 +0100

    Encoder Support & unlock for Kastenwagen

---
+++ keyboards/nopunin10did/kastenwagen1840_chunky/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xC7, 0x54, 0x27, 0xD8, 0xE7, 0x07, 0x5A, 0x8F}
+#define VIAL_UNLOCK_COMBO_ROWS { 2, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 2, 5 }
