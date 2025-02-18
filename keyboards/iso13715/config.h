1838dd2e504d355319c4c56c22df5f1dee538e74
exen904 <diesdas@kurantrubys.de>
2025-02-18 01:22:52 +0100

    added ISO13715

---
+++ keyboards/iso13715/config.h
@@ -0,0 +1,5 @@
+#define WS2812_DI_PIN GP28  // Replace with actual GPIO number
+#define RGBLIGHT_LED_COUNT 3     // Number of LEDs
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 17
+#define RGBLIGHT_VAL_STEP 17
---
+++ keyboards/iso13715/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xB4, 0x00, 0x49, 0x78, 0x83, 0xF3, 0x40, 0x19}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 4 }
+#define BOOTMAGIC_ROW 0
+#define BOOTMAGIC_COLUMN 0
